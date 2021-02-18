eff47b8d6671cf053d316d01455c82a00cea42bb
Tirth Raj Kumar <stirthraj.12345@gmail.com>
2021-02-18 09:36:03 +0530

    version 2.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { ChatEngine} from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 const App=()=>{
+    if(!localStorage.getItem('username')) return <LoginForm/>
     return(
         <ChatEngine
         height="100vh"
         projectID="bc142d4c-43d0-497d-ad15-c6da7c6f365d"
-        userName="Tirthraj"
-        userSecret="12345"
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
         renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
         />
     )
